711d306512a4ff338966291fe39d54238ae49fbb
abdulaziz <abdulazizakbarov065@gmail.com>
2025-05-01 17:42:24 +0500

    a

--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -1,12 +1,9 @@
-"use client";
+import { Button } from "@/components/ui/button";
 import axios from "axios";
-import { useParams, useSearchParams, useRouter } from "next/navigation";
-import Link from "next/link";
-
 import { GetServerSidePropsContext } from "next";
-import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { useParams, useSearchParams } from "next/navigation";
 import CartItem from "@/components/CartItem";
-
 export type CategoriesPage = {
   id: number;
   name: string;
@@ -18,67 +15,68 @@ export type CategoriesPage = {
   imageUrl: string;
   count?: number;
 };
-
-type ObjectType = {
+type objectType = {
+  limit: number;
   items: CategoriesPage[];
   totalItems: number;
   page: number;
-  limit: number;
 };
 
 export async function getServerSideProps(args: GetServerSidePropsContext) {
-  const res = await axios.get(`https://nt.softly.uz/api/front/products`, {
+  console.log(args,'args');
+  const res = await axios.get("https://nt.softly.uz/api/front/products", {
     params: {
       page: args.query.page,
       limit: args.query.limit,
+      categoryId: args.params?.id,
     },
   });
-
+  
   return {
     props: {
       data: res.data,
     },
   };
 }
-
-function Category({ data }: { data: ObjectType }) {
-  const params = useParams();
-  const id = params?.id;
-  const router = useRouter();
+function CategorieProduct({ data }: { data: objectType }) {
   const searchParams = useSearchParams();
+  const page = searchParams.get("page") || 1;
+  const limit = Number(searchParams.get("limit") || 4);
 
-  const page = Number(searchParams.get("page")) || 1;
-  const limit = Number(searchParams.get("limit")) || 4;
-
-  const pages = Math.ceil((data?.totalItems || 0) / (data?.limit || 1));
-
-  if (!id) {
-    return <div>Bunday Id Mavjud Emas</div>;
+  if (!data) {
+    return (
+      <div className="mx-auto container text-center  text-xl ">loading...</div>
+    );
   }
-
+  const pageCount = Math.ceil(data.totalItems / limit);
+  const params = useParams();
+  if (!params) {
+    return <></>;
+  }
   return (
-    <div className="flex flex-col px-12 py-6 container mx-auto">
-      <div className="flex flex-wrap gap-4">
-        {data.items.map((item) => (
-          <CartItem key={item.id} item={item} />
-        ))}
+    <div>
+      <div className="grid grid-cols-4 container w-full mx-auto py-4">
+        {data.items.map((item:any) => {
+          return <CartItem item={item} key={item.id} />;
+        })}
       </div>
 
-      <div className="flex justify-center mt-10">
-        {[...Array(pages)].map((_, index) => {
-          const index_number = index + 1;
+      <div className="flex justify-center gap-2 mb-5">
+        {[...Array(pageCount)].map((_, index) => {
           return (
-            <Link
-              href={`/category/${id}?page=${index_number}&limit=${data.limit}`}
-            >
-              <Button
-                variant={
-                  index_number === Number(data.page) ? "default" : "outline"
-                }
+            <div key={index}>
+              <Link
+                href={`/category/${params.id}?page=${
+                  index + 1
+                }&limit=${limit}`}
               >
-                {index_number}
-              </Button>
-            </Link>
+                <Button
+                  variant={index + 1 === Number(page) ? "outline" : "ghost"}
+                >
+                  {index + 1}
+                </Button>
+              </Link>
+            </div>
           );
         })}
       </div>
@@ -86,4 +84,4 @@ function Category({ data }: { data: ObjectType }) {
   );
 }
 
-export default Category;
+export default CategorieProduct;
